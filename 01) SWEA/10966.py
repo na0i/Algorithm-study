96f009bdcd9e90c31705878ccaff6daf6c7608a1
na_0_i <nayoi1oo5@gmail.com>
2021-10-24 15:29:58 +0900

    SWEA 10966

--- 01) SWEA/10966.py
+++ 01) SWEA/10966.py
@@ -1,20 +1,13 @@
+from collections import deque
 import sys
 sys.stdin = open('10966.txt', 'r')
 
-from collections import deque
 
-def bfs(r, c):
-    myqueue.append((r, c))
-    visited[r][c] = 1
-    dist[r][c] = 0
+def bfs():
+    visited[myqueue[0][0]][myqueue[0][1]] = 1
 
     while myqueue:
         now = myqueue.popleft()
-
-        if land[now[0]][now[1]] == 'W':
-            print(r, c, dist[now[0]][now[1]])
-            print(dist)
-            break
 
         for d in range(4):
             next_r = now[0] + dr[d]
@@ -25,23 +18,35 @@ def bfs(r, c):
                 visited[next_r][next_c] = 1
                 dist[next_r][next_c] = dist[now[0]][now[1]] + 1
 
+    return dist
 
 T = int(input())
 for tc in range(T):
     N, M = map(int, input().split())
-    land = [list(input()) for _ in range(N)]
+    pool = [list(input()) for _ in range(N)]
     visited = [[0] * M for _ in range(N)]
     dist = [[0] * M for _ in range(N)]
     myqueue = deque()
-
+    W_list = []
     dr = [-1, 1, 0, 0]
     dc = [0, 0, -1, 1]
 
     for i in range(N):
         for j in range(M):
-            if land[i][j] == 'L':
-                bfs(i, j)
-                visited = [[0] * M for _ in range(N)]
-                dist = [[0] * M for _ in range(N)]
+            if pool[i][j] == 'W':
+                myqueue.append((i, j))
+                visited[i][j] = 1
 
-    print()
+    bfs()
+
+    ans = 0
+    for i in range(N):
+        for j in range(M):
+            ans += dist[i][j]
+
+    print('#{} {}'.format(tc+1, ans))
+
+# Land에서 Water를 찾으면 시간초과가 났다.
+# 반대로 Water에서 Land를 찾는식으로 코드를 짠다.
+# BFS를 이용한다.
+# deque를 이용하는 것이 시간복잡도에서 유리하다.
