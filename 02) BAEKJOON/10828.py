8bf7e13a032fc15680dd52df2a260eebc563b442
na_0_i <nayoi1oo5@gmail.com>
2021-10-29 00:44:09 +0900

    BAEKJOON 10828

--- 02) BAEKJOON/10828.py
+++ 02) BAEKJOON/10828.py
@@ -1,1 +1,29 @@
+import sys
+
 N = int(input())
+stack = []
+for n in range(N):
+    command = list(sys.stdin.readline().split())
+    if command[0] == 'pop':
+        if stack:
+            print(stack.pop())
+        else:
+            print('-1')
+
+    elif command[0] == 'size':
+        print(len(stack))
+
+    elif command[0] == 'empty':
+        if stack:
+            print(0)
+        else:
+            print(1)
+
+    elif command[0] == 'top':
+        if stack:
+            print(stack[-1])
+        else:
+            print(-1)
+
+    else:
+        stack.append(command[1])
