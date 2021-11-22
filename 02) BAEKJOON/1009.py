21299566fe97496fd0d70431d5b575f303d10da6
na_0_i <nayoi1oo5@gmail.com>
2021-11-22 16:24:14 +0900

    BAEKJOON 1009

---
+++ 02) BAEKJOON/1009.py
@@ -0,0 +1,13 @@
+T = int(input())
+for tc in range(T):
+    a, b = map(int, input().split())
+
+    # data = pow(a, b)
+    # data = str(data)
+    #
+    # if data[-1] == '0':
+    #     print('10')
+    # else:
+    #     print(data[-1])
+
+    
