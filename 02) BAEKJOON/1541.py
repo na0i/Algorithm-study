95bb771f7b13f1b42a61de62259cb32f81a426d0
na_0_i <nayoi1oo5@gmail.com>
2021-11-26 01:13:42 +0900

    BAEKJOON 1541

--- 02) BAEKJOON/1541.py
+++ 02) BAEKJOON/1541.py
@@ -1,1 +1,17 @@
-calcul = input()
+calcul = list(input())
+calcul_list = []
+sign = ['+', '-']
+
+num = ''
+for i in range(len(calcul)):
+    if calcul[i] != sign[0] and calcul[i] != sign[1]:
+        num += calcul[i]
+    else:
+        calcul_list.append(num)
+        calcul_list.append(calcul[i])
+        num = ''
+    if i == len(calcul) - 1:
+        calcul_list.append(num)
+
+gwalho = ['(', ')']
+
