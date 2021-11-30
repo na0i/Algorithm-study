0db6e9f1b8e6bebdf053ed9c2cf701b71f8bc9f4
na_0_i <nayoi1oo5@gmail.com>
2021-11-30 17:50:51 +0900

    BAEKJOON 1541

--- 02) BAEKJOON/1541.py
+++ 02) BAEKJOON/1541.py
@@ -1,17 +1,34 @@
-calcul = list(input())
-calcul_list = []
-sign = ['+', '-']
+# calcul = list(input())
+# calcul_list = []
+# sign = ['+', '-']
+#
+# num = ''
+# for i in range(len(calcul)):
+#     if calcul[i] != sign[0] and calcul[i] != sign[1]:
+#         num += calcul[i]
+#     else:
+#         calcul_list.append(num)
+#         calcul_list.append(calcul[i])
+#         num = ''
+#     if i == len(calcul) - 1:
+#         calcul_list.append(num)
+#
+# gwalho = ['(', ')']
 
-num = ''
-for i in range(len(calcul)):
-    if calcul[i] != sign[0] and calcul[i] != sign[1]:
-        num += calcul[i]
-    else:
-        calcul_list.append(num)
-        calcul_list.append(calcul[i])
-        num = ''
-    if i == len(calcul) - 1:
-        calcul_list.append(num)
+calcul = list(map(str, input().split('-')))
 
-gwalho = ['(', ')']
+answer = 0
+first = 0
+sub = 0
+for c in range(1):
+    num = calcul[c].split('+')
+    for n in range(len(num)):
+        first += int(num[n])
 
+for c in range(1, len(calcul)):
+    num = calcul[c].split('+')
+    for n in range(len(num)):
+        sub += int(num[n])
+
+answer = answer + first - sub
+print(answer)
