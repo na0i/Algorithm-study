bee2e8dc8b51f42c65496f02cfd1baec4ff5b805
na_0_i <nayoi1oo5@gmail.com>
2022-06-09 21:57:36 +0900

    나동빈92

--- 04) 그 외/나동빈_92p_큰 수의 법칙.py
+++ 04) 그 외/나동빈_92p_큰 수의 법칙.py
@@ -3,8 +3,9 @@ sys.stdin = open('나동빈_92p_큰 수의 법칙.txt', 'r')
 
 N, M, K = map(int, input().split())
 number_list = list(map(int, input().split()))
-sorted_number_list = sorted(number_list)
 
+# 1번 풀이
+sorted_number_list = sorted(number_list)
 answer = 0
 cnt = 0
 while cnt < M:
@@ -18,5 +19,20 @@ while cnt < M:
     if cnt < M:
         answer += sorted_number_list[-2]
         cnt += 1
+
+print(answer)
+
+# 2번 풀이
+first = sorted_number_list[-1]
+second = sorted_number_list[-2]
+answer = 0
+
+mapped_number_sum = first * K + second
+mapped_number_length = K + 1
+repeat_cnt = M // mapped_number_length
+remained_cnt = M % mapped_number_length
+
+answer += mapped_number_sum * repeat_cnt
+answer += first * remained_cnt
 
 print(answer)
