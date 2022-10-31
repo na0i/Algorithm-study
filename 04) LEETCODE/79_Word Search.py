a6f8374e00ec120b5188e2b8d75e088be099c337
na_0_i <nayoi1oo5@gmail.com>
2022-10-31 22:38:36 +0900

    LEETCODE 79

---
+++ 04) LEETCODE/79_Word Search.py
@@ -0,0 +1,43 @@
+board = [["A", "A", "A", "A", "A", "A"], ["A", "A", "A", "A", "A", "A"], ["A", "A", "A", "A", "A", "A"], ["A", "A", "A", "A", "A", "A"], ["A", "A", "A", "A", "A", "A"], ["A", "A", "A", "A", "A", "A"]]
+word = "AAAAAAAAAAAABAA"
+# board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]
+# word = "ABCCED"
+# board = [["A","A","A","A","A","A"],["A","A","A","A","A","A"],["A","A","A","A","A","A"],["A","A","A","A","A","A"],["A","A","A","A","A","A"],["A","A","A","A","A","A"]]
+# word = "BAAAAAAAAAAAAAA"
+visited = [[0] * len(board[0]) for _ in range(len(board))]
+dr = [-1, 1, 0, 0]
+dc = [0, 0, -1, 1]
+
+
+def dfs(r, c, word_idx, visit_check):
+    global flag
+
+    if word_idx == len(word) - 1:
+        flag = True
+        return
+
+    else:
+        for d in range(4):
+            next_r = r + dr[d]
+            next_c = c + dc[d]
+
+            if 0 <= next_r < len(board) and 0 <= next_c < len(board[0]) and visit_check[next_r][next_c] == 0 and word_idx < len(word) - 1 and board[next_r][next_c] == word[word_idx+1]:
+                visit_check[next_r][next_c] = 1
+                dfs(next_r, next_c, word_idx+1, visit_check)
+                visit_check[next_r][next_c] = 0
+        return
+
+
+flag = False
+for i in range(len(board)):
+    for j in range(len(board[0])):
+        if flag:
+            break
+
+        elif board[i][j] == word[0]:
+            visited[i][j] = 1
+            dfs(i, j, 0, visited)
+            visited[i][j] = 0
+
+print(flag)
+
